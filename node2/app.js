21b54c3e18cdb1b8577859267228b0d73d32651f
SooMin Kim <Kimsoominj@gmail.com>
2024-07-19 16:21:39 +0900

    0719

--- node2/app.js
+++ node2/app.js
@@ -1,79 +1,260 @@
-// Package 선언부
-require('dotenv').config(); //.env를 config로 사용하겠다
-const express=require('express');
+// Package 
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+
 const nunjucks = require('nunjucks');
-const bodyParser = require('body-parser')   //body parser 추가 1
-const app = express(); //app객체로 express를 사용하겠다
-const port = process.env.SERVER_PORT || 3000; // .env의 포트 번호를 가져옴, 안되면 3000으로 가져옴
+const bodyParser = require('body-parser')
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+// session 저장소 지정(메모리)
+const MemoryStore = require("memorystore")(session);
+// Passport lib 
+const passport = require("passport"),
+    LocalStrategy = require("passport-local").Strategy;
 
 // Database 연동
 var db_connect = require('./db/db_connect');
 var db_sql = require('./db/db_sql');
 
+// My util
+var goto = require('./util/goto');
 
-
-
-//HTML파일 위치 views(views를 html에 넣겠다고 한 부분)
-nunjucks.configure('views',{ // 화면에 관련된걸 views에 넣겠다
-    express:app, //끝났는지 아닌지 모르니 ;찍기
+// CORS 지정
+const cors = require("cors");
+app.use(cors());
+// HTML 파일 위치 views
+nunjucks.configure('views', {
+    express: app,
 });
 
-// Html 환경설정, post, public 디렉토리 설정
-app.set('view engine', 'html'); //view엔진을 html로 화면 구성하겠다
-app.use(bodyParser.urlencoded({extended:false})); //post방식 확인
-app.use(express.static('public')); //public: image, css
-//선언부 끝
+// html 환경 설정, post, public
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ extended: false })); //객체 들어감. 추가 2 
+app.use(express.static('public'));
 
-//Controller - 화면 요청 시 동작하는 부분(요청을 받아서 처리)
-//127.0.0.1:3000/ 이걸 치면 get가 받아서 처리
-app.get('/', (req,res)=>{
-    res.render('index'); //index가 얘는 메인
+
+// Session 선언
+app.use(
+    session({
+        secret: "secret key",
+        resave: false,
+        saveUninitialized: true,
+
+        store: new MemoryStore({
+            checkPeriod: 86400000, // 24 hours (= 24 * 60 * 60 * 1000 ms)
+        })
+    })
+);
+
+
+// 2. Passport를 이용한 로그인 처리 ---------------------------------------------------------------------------------------
+
+// passport 초기화 및 session 연결
+app.use(passport.initialize());
+app.use(passport.session());
+
+// login이 최초로 성공했을 때만 호출되는 함수
+// done(null, user.id)로 세션을 초기화 한다.
+passport.serializeUser(function (req, user, done) {
+    console.log('serializeUser' + user);
+    console.log('serializeUser' + user.id);
+    console.log('serializeUser' + user.name);
+    console.log('serializeUser' + user.acc);
+
+    done(null, user);
+});
+
+// 사용자가 페이지를 방문할 때마다 호출되는 함수
+// done(null, id)로 사용자의 정보를 각 request의 user 변수에 넣어준다.
+passport.deserializeUser(function (req, user, done) {
+    console.log('Login User' + user.name + ' ' + user.id);
+    done(null, user);
+});
+
+// local login 전략을 세우는 함수
+// client에서 전송되는 변수의 이름이 각각 id, pw이므로 
+// usernameField, passwordField에서 해당 변수의 값을 받음
+// 이후부터는 username, password에 각각 전송받은 값이 전달됨
+// 위에서 만든 login 함수로 id, pw가 유효한지 검출
+// 여기서 로그인에 성공하면 위의 passport.serializeUser 함수로 이동
+
+passport.use(
+    new LocalStrategy(
+        {
+            usernameField: "id",
+            passwordField: "pwd",
+        },
+        function (userid, password, done) {
+            console.log('--------------------------' + userid);
+            console.log('--------------------------' + password);
+
+            conn = db_connect.getConnection();
+            conn.query(db_sql.cust_select_one, [userid], (err, row, fields) => {
+
+                if (err) throw err;
+
+                let result = 0;
+                console.log('--------------------------' + row[0]['pwd']);
+
+                let name = row[0]['name'];
+                let acc = row[0]['acc'];
+
+                if (row[0] == undefined) {
+                    return done(null, false, { message: "Login Fail " });
+                } else if (row[0]['pwd'] != password) {
+                    return done(null, false, { message: "Login Fail " });
+                } else {
+                    return done(null, { id: userid, name: name, acc: acc });
+                }
+
+            });
+
+        }
+    )
+);
+
+// login 요청이 들어왔을 때 성공시 / 로, 실패시 /login 으로 리다이렉트
+app.post(
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/loginerror",
+    })
+);
+
+// 2. Passport를 이용한 로그인 처리 끝---------------------------------------------------------------------------------------
+app.get('/loginerror', (req, res) => {
+    res.render('index', {
+        center: 'loginerror'
+    })
 })
 
-//login
-app.get('/login', (req,res)=>{
-    res.render('index', {'center':'login'});
+
+
+
+// Controller
+// 127.0.0.1/
+
+app.get('/', (req, res) => {
+    goto.go(req, res, undefined);
+});
+// Login 화면
+app.get('/login', (req, res) => {
+    goto.go(req, res, { 'center': 'login' });
+});
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 })
+// Register 화면
+app.get('/register', (req, res) => {
+    goto.go(req, res, { 'center': 'register' });
+});
+app.post('/registerimpl', (req, res) => {
+    // 입력값 받기
+    let id = req.body.id;
+    let pwd = req.body.pwd;
+    let name = req.body.name;
+    let acc = req.body.acc;
+    console.log(id + ' ' + pwd + ' ' + name + ' ' + acc);
+    // DB에 입력 하고 center에 회원가입을 축하합니다. 출력
+    let values = [id, pwd, name, acc];
+    conn = db_connect.getConnection();
 
-//register
-app.get('/register', (req,res)=>{
-    res.render('index', {'center':'register'});
-})
+    conn.query(db_sql.cust_insert, values, (e, result, fields) => {
+        try {
+            if (e) {
+                console.log('Insert Error');
+                console.log(e);
+                throw e;
+            } else {
+                console.log('Insert OK !');
+                goto.go(req, res, { 'center': 'registerok', 'name': name });
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            db_connect.close(conn);
+        }
+    });
+});
+// Map 화면
+app.get('/map', (req, res) => {
+    goto.go(req, res, { 'center': 'map' });
+});
+app.get('/map2', (req, res) => {
+    goto.go(req, res, { 'center': 'map2' });
+});
+// Chart 화면
+app.get('/chart', (req, res) => {
+    goto.go(req, res, { 'center': 'chart' });
+});
+app.get('/chart2', (req, res) => {
+    goto.go(req, res, { 'center': 'chart2' });
+});
 
-//map
-app.get('/map', (req,res)=>{
-    res.render('index', {'center':'map'});
-})
+// MyInfo 화면
+app.get('/myinfo', (req, res) => {
+    let id = req.query.id;
+    conn = db_connect.getConnection();
+    conn.query(db_sql.cust_select_one, id, (err, result, fields) => {
+        try {
+            if (err) {
+                console.log('Select Error');
+                throw err;
+            } else {
+                console.log(result);
+                custinfo = result[0];
+                console.log(custinfo);
+                goto.go(req, res, { 'center': 'myinfo', 'custinfo': custinfo });
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            db_connect.close(conn);
+        }
+    });
+});
+app.post("/updateimpl", (req, res) => {
+    let id = req.body.id;
+    let pwd = req.body.pwd;
+    let name = req.body.name;
+    let acc = req.body.acc;
+    console.log(id + ' ' + pwd + ' ' + name + ' ' + acc);
+    let values = [pwd, name, acc, id];
+    conn = db_connect.getConnection();
 
-//map2
-app.get('/map2', (req,res)=>{
-    res.render('index', {'center':'map2'});
-})
+    conn.query(db_sql.cust_update, values, (e, result, fields) => {
+        try {
+            if (e) {
+                console.log('Update Error');
+                console.log(e);
+                throw e;
+            } else {
+                console.log('Update OK !');
+                res.redirect('/myinfo?id=' + id);
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            db_connect.close(conn);
+        }
 
-//chart
-app.get('/chart', (req,res)=>{
-    res.render('index', {'center':'chart'});
-})
+    });
+});
+// Detail 화면
+app.get('/detail', (req, res) => {
+    goto.go(req, res, { 'center': 'detail' });
+});
 
-//chart2
-app.get('/chart2', (req,res)=>{
-    res.render('index', {'center':'chart2'});
-})
-
-//detail
-app.get('/detail', (req,res)=>{
-    res.render('index', {'center':'detail'});
-})
-
-
-//Router 생성하는 곳
-const cust = require('./routes/cust'); //cust가 들어오면 이 줄에 있는 cust가 처리
+// Router 
+const cust = require('./routes/cust');
+const item = require('./routes/item');
 app.use('/cust', cust);
-const item = require('./routes/item'); 
 app.use('/item', item);
 
-
-//브라우저에서 요청을 계속 보내는
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server start port:${port}`)
 })
--- node2/routes/cust.js
+++ node2/routes/cust.js
@@ -1,93 +1,153 @@
-const express=require('express');
+const express = require('express');
 const app = express();
 const router = express.Router();
 const nunjucks = require('nunjucks');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 
 // Database 연동
 var db_connect = require('../db/db_connect');
-var db_sql = require('../db/db_sql');
+var db_sql = require('../db/db_sql'); 
 // /cust
 router
-    .get("/",(req,res)=>{ //127.0.0.1/cust 라고 호출했을 때 "/"가 호출됨
+    .get("/", (req, res) => { //127.0.0.1/cust 라고 호출했을 때 "/"가 호출됨
         conn = db_connect.getConnection();
         conn.query(db_sql.cust_select, function (err, result, fields) {
             console.log(result);
-            res.render('index', { center:'cust/list', datas:result });
+            res.render('index', { center: 'cust/list', datas: result });
             db_connect.close(conn);
         });
     })
-    .get("/add",(req,res)=>{   // 127.0.0.1/cust/add
-        res.render('index',{'center':'cust/add'});
+    .get("/add", (req, res) => {   // 127.0.0.1/cust/add
+        res.render('index', { 'center': 'cust/add' });
     })
-
-    .get("/deleteimpl",(req,res)=>{ 
-        res.render('index',{'center':'cust/list'});
-    })
-
-    .get("/detail",(req,res)=>{   // 127.0.0.1/cust/detail
+    .get("/deleteimpl", (req, res) => {   // 127.0.0.1/cust/deleteimpl
         let id = req.query.id; //get방식일땐 quary에서 가져옴
         conn = db_connect.getConnection();
+
+        conn.query(db_sql.cust_delete, id, (err, result, fields) => {
+            try {
+                if (err) {
+                    console.log('Delete Error');
+                    throw err;
+                } else {
+                    res.redirect('/cust');
+                }
+            } catch (e) {
+                console.log(e);
+            } finally {
+                db_connect.close(conn);
+            }
+        });
+    })
+    .get("/detail", (req, res) => {   // 127.0.0.1/cust/detail
+        let id = req.query.id;
+        conn = db_connect.getConnection();
         conn.query(db_sql.cust_select_one, id, (err, result, fields) => {
-            if(err){
-                console.log('Select Error');
-                console.log('Error Message:')+e;
+            try {
+                if (err) {
+                    console.log('Select Error');
+                    throw err;
+                } else {
+                    console.log(result);
+                    custinfo = result[0];
+                    console.log(custinfo);
+                    res.render('index', { 'center': 'cust/detail', 'custinfo': custinfo });
+                }
+            } catch (e) {
+                console.log(e);
+            } finally {
                 db_connect.close(conn);
-            }else{
-                console.log(result);
-                custinfo = result[0];
-                console.log(custinfo);
-
-                db_connect.close(conn);
-                res.render('index',{'center':'cust/detail', 'custinfo':custinfo});
             }
         });
     })
 
-    // post
-    // 삭제
-    .post("/deleteimpl",(req,res)=>{
+
+    
+
+    .post("/updateimpl", (req, res) => {
         let id = req.body.id; //post방식일땐 body에서 가져옴
         let pwd = req.body.pwd;
         let name = req.body.name;
         let acc = req.body.acc;
-        console.log(id+' '+pwd+' '+name+' '+acc);
-        let values = [id,pwd,name,acc];
+        console.log(id + ' ' + pwd + ' ' + name + ' ' + acc);
+        let values = [id, pwd, name, acc, id]; //db_sql순서 맞춰줘야함
+
         conn = db_connect.getConnection();
 
-        conn.query(db_sql.cust_insert, values, (e, result, fields) => {
-            if(e){
-                console.log('Insert Error');
+        conn.query(db_sql.cust_update, values, (e, result, fields) => {
+            try {
+                if (e) {
+                    console.log('Insert Error');
+                    console.log(e);
+                    throw e;
+                } else {
+                    console.log('Insert OK !');
+                    res.redirect('/cust/detail?id=' + id); //수정을 하고 디테일로 가는데 id를 가진 애들을 모아와라
+                }
+            } catch (e) {
                 console.log(e);
+            } finally {
                 db_connect.close(conn);
-            }else{
-                console.log('Insert OK !');
-                db_connect.close(conn);
-                res.redirect('/cust'); //기존에 만들었던 페이지 호출
             }
+
         });
     })
 
-    //추가
-    .post("/addimpl",(req,res)=>{
+    .post("/registerimpl", (req, res) => {
         let id = req.body.id; //post방식일땐 body에서 가져옴
         let pwd = req.body.pwd;
         let name = req.body.name;
         let acc = req.body.acc;
-        console.log(id+' '+pwd+' '+name+' '+acc);
-        let values = [id,pwd,name,acc];
+        console.log(id + ' ' + pwd + ' ' + name + ' ' + acc);
+        let values = [id, pwd, name, acc, id]; //db_sql순서 맞춰줘야함
+
         conn = db_connect.getConnection();
 
         conn.query(db_sql.cust_insert, values, (e, result, fields) => {
-            if(e){
-                console.log('Insert Error');
+            try {
+                if (e) {
+                    console.log('Insert Error');
+                    console.log(e);
+                    throw e;
+                } else {
+                    console.log('Insert OK !');
+                    res.render('index', {'center':'registerok','name':name});
+                }
+            } catch (e) {
                 console.log(e);
+            } finally {
                 db_connect.close(conn);
-            }else{
-                console.log('Insert OK !');
-                db_connect.close(conn);
-                res.redirect('/cust'); //기존에 만들었던 페이지 호출
             }
+
+        });
+    })
+
+
+    .post("/addimpl", (req, res) => {
+        let id = req.body.id;
+        let pwd = req.body.pwd;
+        let name = req.body.name;
+        let acc = req.body.acc;
+        console.log(id + ' ' + pwd + ' ' + name + ' ' + acc);
+        let values = [id, pwd, name, acc];
+        conn = db_connect.getConnection();
+
+        conn.query(db_sql.cust_insert, values, (e, result, fields) => {
+            try {
+                if (e) {
+                    console.log('Insert Error');
+                    console.log(e);
+                    throw e;
+                } else {
+                    console.log('Insert OK !');
+                    res.redirect('/cust');  //기존에 만들었던 페이지 호출
+                }
+            } catch (e) {
+                console.log(e);
+            } finally {
+                db_connect.close(conn);
+            }
+
         });
     });
 
